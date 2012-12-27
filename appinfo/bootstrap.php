d29827f307f9823cce863f24255a38b8a1cfee29
Georg Ehrke <dev@georgswebsite.de>
2012-12-27 19:14:37 +0100

    fix usage of classpath in calendar app

--- appinfo/bootstrap.php
+++ appinfo/bootstrap.php
@@ -7,42 +7,42 @@
  */
 namespace OCA\Calendar;
 //set classpath
-\OC::$CLASSPATH['OCA\Calendar'] = 'apps/calendar/lib/calendar.php';
+\OC::$CLASSPATH['OCA\Calendar'] = 'calendar/lib/calendar.php';
 //backend classes
 //\OCA\Calendar\Util::registerEnabledBackends();
-\OC::$CLASSPATH['OCA\Calendar\Backend\Backend'] = 'apps/calendar/lib/backends/backend.php';
-\OC::$CLASSPATH['OCA\Calendar\Backend\CalendarInterface'] = 'apps/calendar/lib/backends/interface.php';
-\OC::$CLASSPATH['OCA\Calendar\Backend\Database'] = 'apps/calendar/lib/backends/database.php';
-\OC::$CLASSPATH['OCA\Calendar\Backend\LocalStorage'] = 'apps/calendar/lib/backends/localstorage.php';
-\OC::$CLASSPATH['OCA\Calendar\Backend\Share'] = 'apps/calendar/lib/backends/share.php';
-\OC::$CLASSPATH['OCA\Calendar\Backend\WebCal'] = 'apps/calendar/lib/backends/webcal.php';
+\OC::$CLASSPATH['OCA\Calendar\Backend\Backend'] = 'calendar/lib/backends/backend.php';
+\OC::$CLASSPATH['OCA\Calendar\Backend\CalendarInterface'] = 'calendar/lib/backends/interface.php';
+\OC::$CLASSPATH['OCA\Calendar\Backend\Database'] = 'calendar/lib/backends/database.php';
+\OC::$CLASSPATH['OCA\Calendar\Backend\LocalStorage'] = 'calendar/lib/backends/localstorage.php';
+\OC::$CLASSPATH['OCA\Calendar\Backend\Share'] = 'calendar/lib/backends/share.php';
+\OC::$CLASSPATH['OCA\Calendar\Backend\WebCal'] = 'calendar/lib/backends/webcal.php';
 //export and import
-\OC::$CLASSPATH['OCA\Calendar\Export'] = 'apps/calendar/lib/helper/export.php';
-\OC::$CLASSPATH['OCA\Calendar\Import'] = 'apps/calendar/lib/helper/import.php';
+\OC::$CLASSPATH['OCA\Calendar\Export'] = 'calendar/lib/helper/export.php';
+\OC::$CLASSPATH['OCA\Calendar\Import'] = 'calendar/lib/helper/import.php';
 //helper classes
-\OC::$CLASSPATH['OCA\Calendar\Attendees'] = 'apps/calendar/lib/helper/attendees.php';
-\OC::$CLASSPATH['OCA\Calendar\Hooks'] = 'apps/calendar/lib/helper/hooks.php';
-\OC::$CLASSPATH['OCA\Calendar\Recurrence'] = 'apps/calendar/lib/helper/recurrence.php';
-\OC::$CLASSPATH['OCA\Calendar\Reminder'] = 'apps/calendar/lib/helper/reminder.php';
-\OC::$CLASSPATH['OCA\Calendar\Repeat'] = 'apps/calendar/lib/helper/repeat.php';
-\OC::$CLASSPATH['OCA\Calendar\Search'] = 'apps/calendar/lib/search/search.php';
-\OC::$CLASSPATH['OCA\Calendar\Util'] = 'apps/calendar/lib/helper/util.php';
+\OC::$CLASSPATH['OCA\Calendar\Attendees'] = 'calendar/lib/helper/attendees.php';
+\OC::$CLASSPATH['OCA\Calendar\Hooks'] = 'calendar/lib/helper/hooks.php';
+\OC::$CLASSPATH['OCA\Calendar\Recurrence'] = 'calendar/lib/helper/recurrence.php';
+\OC::$CLASSPATH['OCA\Calendar\Reminder'] = 'calendar/lib/helper/reminder.php';
+\OC::$CLASSPATH['OCA\Calendar\Repeat'] = 'calendar/lib/helper/repeat.php';
+\OC::$CLASSPATH['OCA\Calendar\Search'] = 'calendar/lib/search/search.php';
+\OC::$CLASSPATH['OCA\Calendar\Util'] = 'calendar/lib/helper/util.php';
 //sabredav implementation
-\OC::$CLASSPATH['OC_Connector_Sabre_CalDAV'] = 'apps/calendar/lib/sabre/backend.php';
-\OC::$CLASSPATH['OC_Connector_Sabre_CalDAV_CalendarRoot'] = 'apps/calendar/lib/sabre/calendarroot.php';
-\OC::$CLASSPATH['OC_Connector_Sabre_CalDAV_UserCalendars'] = 'apps/calendar/lib/sabre/usercalendars.php';
-\OC::$CLASSPATH['OC_Connector_Sabre_CalDAV_Calendar'] = 'apps/calendar/lib/sabre/calendar.php';
-\OC::$CLASSPATH['OC_Connector_Sabre_CalDAV_CalendarObject'] = 'apps/calendar/lib/sabre/object.php';
+\OC::$CLASSPATH['OC_Connector_Sabre_CalDAV'] = 'calendar/lib/sabre/backend.php';
+\OC::$CLASSPATH['OC_Connector_Sabre_CalDAV_CalendarRoot'] = 'calendar/lib/sabre/calendarroot.php';
+\OC::$CLASSPATH['OC_Connector_Sabre_CalDAV_UserCalendars'] = 'calendar/lib/sabre/usercalendars.php';
+\OC::$CLASSPATH['OC_Connector_Sabre_CalDAV_Calendar'] = 'calendar/lib/sabre/calendar.php';
+\OC::$CLASSPATH['OC_Connector_Sabre_CalDAV_CalendarObject'] = 'calendar/lib/sabre/object.php';
 //sharing implementation
-\OC::$CLASSPATH['OC_Share_Backend_Calendar'] = 'apps/calendar/lib/share/calendar.php';
-\OC::$CLASSPATH['OC_Share_Backend_Event'] = 'apps/calendar/lib/share/event.php';
+\OC::$CLASSPATH['OC_Share_Backend_Calendar'] = 'calendar/lib/share/calendar.php';
+\OC::$CLASSPATH['OC_Share_Backend_Event'] = 'calendar/lib/share/event.php';
 //objects for internel information interchange
-\OC::$CLASSPATH['OCA\Calendar\Objects\Calendar'] = 'apps/calendar/lib/objects/calendar.php';
-\OC::$CLASSPATH['OCA\Calendar\Objects\Event'] = 'apps/calendar/lib/objects/event.php';
-\OC::$CLASSPATH['OCA\Calendar\Objects\Journal'] = 'apps/calendar/lib/objects/journal.php';
-\OC::$CLASSPATH['OCA\Calendar\Objects\Todo'] = 'apps/calendar/lib/objects/todo.php';
+\OC::$CLASSPATH['OCA\Calendar\Objects\Calendar'] = 'calendar/lib/objects/calendar.php';
+\OC::$CLASSPATH['OCA\Calendar\Objects\Event'] = 'calendar/lib/objects/event.php';
+\OC::$CLASSPATH['OCA\Calendar\Objects\Journal'] = 'calendar/lib/objects/journal.php';
+\OC::$CLASSPATH['OCA\Calendar\Objects\Todo'] = 'calendar/lib/objects/todo.php';
 
-\OC::$CLASSPATH['Sabre\VObject\Component'] = 'apps/calendar/3rdparty/VObject/Component.php';
+\OC::$CLASSPATH['Sabre\VObject\Component'] = 'calendar/3rdparty/VObject/Component.php';
 
 //register for hooks
 \OCP\Util::connectHook('OC_User', 'post_createUser', 'OC_Calendar_Hooks', 'createUser');
