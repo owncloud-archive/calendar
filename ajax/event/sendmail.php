d8ef58ca8a4fb6b7e745dca53b9d14f671771b5a
visithaWB <visithauom@gmail.com>
2013-04-16 01:37:14 +0530

    this is new ajax file created to send event data to back end for the email creation

---
+++ ajax/event/sendmail.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Copyright (c) 2011 Bart Visscher <bartv@thisnet.nl>
+ * This file is licensed under the Affero General Public License version 3 or
+ * later.
+ * See the COPYING-README file.
+ */
+
+
+OCP\JSON::checkLoggedIn();
+OCP\JSON::checkAppEnabled('calendar');
+OCP\JSON::callCheck();
+
+$id = $_POST['id'];
+$eventId = $_POST['eventId'];
+$location = $_POST['location'];
+$description = $_POST['description'];
+$dtstart = $_POST['dtstart'];
+$dtend = $_POST['dtend'];
+
+try {
+	//OC_Calendar_Object::sendMail();
+	OC_Calendar_App::sendEmails($eventId, $location, $description, $dtstart, $dtend);
+} catch(Exception $e) {
+	OCP\JSON::error(array('data' => array('message'=>$e->getMessage())));
+	exit;
+}
+
+OCP\JSON::success();
