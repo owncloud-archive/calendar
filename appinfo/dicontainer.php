90d0ace7b67bdb7ec07a201e9abb0634969b43fa
Georg Ehrke <georg@ownCloud.com>
2013-05-01 21:12:14 +0200

    cleanup dicontainer.php

--- appinfo/dicontainer.php
+++ appinfo/dicontainer.php
@@ -22,22 +22,22 @@ class DIContainer extends \OCA\AppFramework\DependencyInjection\DIContainer {
 		$this['BackendController'] = $this->share(function($c){
 			return new Controller\Backend($c['API'], $c['Request'], $c['BackendMapper']);
 		});
-		
+
 		//controller for calendars
 		$this['CalendarController'] = $this->share(function($c){
 			return new Controller\Calendar($c['API'], $c['Request'], $c['CachedCalendarMapper'], $c['BackendController'], $c['ObjectController']);
 		});
-		
+
 		//controller for objects like events, journals, todos
 		$this['ObjectController'] = $this->share(function($c){
 			return new Controller\Object($c['API'], $c['Request'], $c['CachedObjectMapper'], $c['BackendController'], $c['CalendarController']);
 		});
-		
+
 		//controller for settings
 		$this['SettingsController'] = $this->share(function($c){
 			return new Controller\Settings($c['API'], $c['Request']);
 		});
-		
+
 		//controller for view
 		$this['ViewController'] = $this->share(function($c){
 			return new Controller\View($c['API'], $c['Request'], $c['BackendController']);
@@ -50,12 +50,12 @@ class DIContainer extends \OCA\AppFramework\DependencyInjection\DIContainer {
 		$this['BackendMapper'] = $this->share(function($c){
 			return new Mapper\Backend($c['API']);
 		});
-				
+
 		//mapper for cached calendars
 		$this['CachedCalendarMapper'] = $this->share(function($c){
 			return new Mapper\CachedCalendar($c['API']);
 		});
-		
+
 		//mapper for cached objects like events, journals, todos
 		$this['CachedObjectMapper'] = $this->share(function($c){
 			return new Mapper\CachedObject($c['API']);
