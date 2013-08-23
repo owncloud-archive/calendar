a35ad40aadc5d034d6953c827de6d074f15f3618
petemcfarlane <peterjohnmcfarlane@gmail.com>
2013-08-23 10:51:50 +0100

    Update settingswrapper to make use of unified template, choosecalendar and settings)
    Issue #126

--- settingswrapper.php
+++ settingswrapper.php
@@ -1,3 +1,14 @@
 <?php
-require_once('calendar.php');
-require_once('settings.php');
+
+$l10n = OC_L10N::get('calendar');
+OCP\JSON::checkLoggedIn();
+OCP\JSON::checkAppEnabled('calendar');
+
+$tmpl = new OCP\Template('calendar', 'part.choosecalendar.settings');
+$timezone=OCP\Config::getUserValue(OCP\USER::getUser(),'calendar','timezone','');
+$tmpl->assign('timezone',$timezone);
+$tmpl->assign('timezones',DateTimeZone::listIdentifiers());
+$tmpl->assign('calendars', OC_Calendar_Calendar::allCalendars(OCP\USER::getUser()), false);
+
+OCP\Util::addscript('calendar','settings');
+$tmpl->printPage();
