c84a5fcf5db8c26e60315c9d8ec7166e5d7a8ac7
Steffen Lindner <mail@steffen-lindner.de>
2013-09-27 16:46:21 +0200

    Fix indent, please

--- lib/share/event.php
+++ lib/share/event.php
@@ -30,16 +30,16 @@ class OC_Share_Backend_Event implements OCP\Share_Backend {
 	public function formatItems($items, $format, $parameters = null) {
 		$events = array();
 		if ($format == self::FORMAT_EVENT) {
-            foreach ($items as $item) {
-                $event = OC_Calendar_Object::find($item['item_source']);
-                if ($event == False) {  
-                    \OCP\Util::writeLog('calendar', __METHOD__.', Missing event: ' . $item['item_target'], \OCP\Util::DEBUG);
-                }
-                else {
-                    $event['summary'] = $item['item_target'];
-                    $event['permissions'] = $item['permissions'];
-                    $events[] = $event;
-                }
+			foreach ($items as $item) {
+				$event = OC_Calendar_Object::find($item['item_source']);
+				if ($event == False) {
+					\OCP\Util::writeLog('calendar', __METHOD__.', Missing event: ' . $item['item_target'], \OCP\Util::DEBUG);
+				}
+				else {
+					$event['summary'] = $item['item_target'];
+					$event['permissions'] = $item['permissions'];
+					$events[] = $event;
+				}
 			}
 		}
 		return $events;
