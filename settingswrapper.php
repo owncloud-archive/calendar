d0b0147413dd15f690f7ec552d22d15d2481abaa
Georg Ehrke <developer@georgehrke.com>
2013-10-03 17:48:47 +0200

    update settings dialog

--- settingswrapper.php
+++ settingswrapper.php
@@ -1,10 +1,9 @@
 <?php
-
 $l10n = OC_L10N::get('calendar');
 OCP\JSON::checkLoggedIn();
 OCP\JSON::checkAppEnabled('calendar');
 
-$tmpl = new OCP\Template('calendar', 'part.choosecalendar.settings');
+$tmpl = new OCP\Template('calendar', 'settings');
 $timezone=OCP\Config::getUserValue(OCP\USER::getUser(),'calendar','timezone','');
 $tmpl->assign('timezone',$timezone);
 $tmpl->assign('timezones',DateTimeZone::listIdentifiers());
