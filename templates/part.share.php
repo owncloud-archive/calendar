1cb301650a0e7ff9ee5ce5956ed9864a426578b1
Michał 'rysiek' Woźniak <rysiek@fwioo.pl>
2014-01-24 04:44:02 +0100

    interface and css more or less ready

--- templates/part.share.php
+++ templates/part.share.php
@@ -20,23 +20,23 @@ $sharedwithByEvent = OCP\Share::getItemShared('event', $eventid);
 
 
 if(is_array($sharedwithByCalendar)) {
-	foreach($sharedwithByCalendar as $share) {
-		if($share['share_type'] == OCP\Share::SHARE_TYPE_USER || $share['share_type'] == OCP\Share::SHARE_TYPE_GROUP) {
-			$calsharees[] = $share;
+  foreach($sharedwithByCalendar as $share) {
+    if($share['share_type'] == OCP\Share::SHARE_TYPE_USER || $share['share_type'] == OCP\Share::SHARE_TYPE_GROUP) {
+      $calsharees[] = $share;
     // public link-sharing
-		} elseif($share['share_type'] == OCP\Share::SHARE_TYPE_LINK) {
+    } elseif($share['share_type'] == OCP\Share::SHARE_TYPE_LINK) {
       $linkSharedCalendar = true;
-		}
-	}
+    }
+  }
 }
 if(is_array($sharedwithByEvent)) {
-	foreach($sharedwithByEvent as $share) {
-		if($share['share_type'] == OCP\Share::SHARE_TYPE_USER || $share['share_type'] == OCP\Share::SHARE_TYPE_GROUP) {
-			$eventsharees[] = $share;
-		} else {
+  foreach($sharedwithByEvent as $share) {
+    if($share['share_type'] == OCP\Share::SHARE_TYPE_USER || $share['share_type'] == OCP\Share::SHARE_TYPE_GROUP) {
+      $eventsharees[] = $share;
+    } else {
       $linkShare = $share;
-		}
-	}
+    }
+  }
 }
 
 /* sharing an event internally */
@@ -44,40 +44,34 @@ $tmpl = new OCP\Template('calendar', 'part.internalshare');
 $tmpl->assign('item_id', $_['eventid']);
 $tmpl->assign('item_type', 'event');
 $tmpl->assign('permissions', $_['permissions']);
-<<<<<<< HEAD
 $tmpl->assign('basic_edit_options', true);
 $tmpl->assign('shared_with', $eventsharees);
-=======
-$tmpl->assign('link_share', $linkShare);
-//$tmpl->assign('shared', $shared);
->>>>>>> a70b987... implementing a decent sharing interface
 $tmpl->printpage();
 ?><strong><?php p($l->t('Shared via calendar')); ?></strong>
-
 <ul class="sharedby calendarlist">
 <?php foreach($calsharees as $sharee): ?>
-	<li data-share-with="<?php p($sharee['share_with']); ?>"
-		data-item="<?php p($calid); ?>"
-		data-item-type="calendar"
-		data-link="true"
-		data-permissions="<?php p($sharee['permissions']); ?>"
-		data-share-type="<?php p($sharee['share_type']); ?>">
-		<?php p($sharee['share_with'] . ($sharee['share_type'] == OCP\Share::SHARE_TYPE_GROUP ? ' (group)' : '')); ?>
-		<span class="shareactions">
-			<label>
-				<input class="update" type="checkbox"
-					<?php p(($sharee['permissions'] & OCP\PERMISSION_UPDATE?'checked="checked"':''))?>
-					disabled="disabled">
-				<?php p($l->t('can edit')); ?>
-			</label>
-			<label>
-				<input class="share" type="checkbox"
-					<?php p(($sharee['permissions'] & OCP\PERMISSION_SHARE?'checked="checked"':''))?>
-					disabled="disabled">
-				<?php p($l->t('can share')); ?>
-			</label>
-		</span>
-	</li>
+  <li data-share-with="<?php p($sharee['share_with']); ?>"
+    data-item="<?php p($calid); ?>"
+    data-item-type="calendar"
+    data-link="true"
+    data-permissions="<?php p($sharee['permissions']); ?>"
+    data-share-type="<?php p($sharee['share_type']); ?>">
+    <?php p($sharee['share_with'] . ($sharee['share_type'] == OCP\Share::SHARE_TYPE_GROUP ? ' (group)' : '')); ?>
+    <span class="shareactions">
+      <label>
+        <input class="update" type="checkbox"
+          <?php p(($sharee['permissions'] & OCP\PERMISSION_UPDATE?'checked="checked"':''))?>
+          disabled="disabled">
+        <?php p($l->t('can edit')); ?>
+      </label>
+      <label>
+        <input class="share" type="checkbox"
+          <?php p(($sharee['permissions'] & OCP\PERMISSION_SHARE?'checked="checked"':''))?>
+          disabled="disabled">
+        <?php p($l->t('can share')); ?>
+      </label>
+    </span>
+  </li>
 <?php endforeach; ?>
 <?php if ($linkSharedCalendar): ?>
   <li data-share-with=""
@@ -92,8 +86,8 @@ $tmpl->printpage();
 <?php endif; ?>
 </ul>
 <?php if(!$calsharees and !$linkSharedCalendar) {
-	$nobody = $l->t('Not shared with anyone via calendar');
-	print_unescaped('<div>' . OC_Util::sanitizeHTML($nobody) . '</div>');
+  $nobody = $l->t('Not shared with anyone via calendar');
+  print_unescaped('<div>' . OC_Util::sanitizeHTML($nobody) . '</div>');
 } ?>
 
 <?php p($l->t('Visibility to people shared with'));?>
