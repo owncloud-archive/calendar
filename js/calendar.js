e282ec846a0afc205fd0f1b709a4b63e03d8fc3b
Georg Ehrke <developer@georgehrke.com>
2014-02-04 21:55:50 +0100

    properly urlencode all caldav links
    Conflicts:
    	templates/settings.php

--- js/calendar.js
+++ js/calendar.js
@@ -290,7 +290,7 @@ Calendar={
 			}
 		},
 		showCalDAVUrl:function(username, calname){
-			$('#caldav_url').val(totalurl + '/' + username + '/' + calname);
+			$('#caldav_url').val(totalurl + '/' + encodeURIComponent(username) + '/' + encodeURIComponent(calname));
 			$('#caldav_url').show();
 			$("#caldav_url_close").show();
 		},
