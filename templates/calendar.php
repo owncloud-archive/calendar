aca02549a00d5f7716c3164b3121521c02d49040
jbtbnl <jbtbnl@users.noreply.github.com>
2014-03-08 10:59:18 +0100

    Don't use tables for settings

--- templates/calendar.php
+++ templates/calendar.php
@@ -46,110 +46,70 @@
 	
 	<!-- Start of settings -->
 	<div id="app-settings">
-			<div id="app-settings-header">
-				<button class="settings-button generalsettings" tabindex="0"></button>
-			</div>
-			<div id="app-settings-content">
-				<div id="general">
-					<table class="nostyle">
-						<tr>
-							<td>
-								<label for="timezone" class="bold"><?php p($l->t('Timezone'));?></label>
-							</td>
-						</tr>
-						<tr>
-							<td>
-								<select id="timezone" name="timezone">
-								<?php
-								$continent = '';
-								foreach($_['timezones'] as $timezone):
-									$ex=explode('/', $timezone, 2);//obtain continent,city
-									if (!isset($ex[1])) {
-										$ex[1] = $ex[0];
-										$ex[0] = "Other";
-									}
-									if ($continent!=$ex[0]):
-										if ($continent!="") print_unescaped('</optgroup>');
-										print_unescaped('<optgroup label="'.OC_Util::sanitizeHTML($ex[0]).'">');
-									endif;
-									$city=strtr($ex[1], '_', ' ');
-									$continent=$ex[0];
-									print_unescaped('<option value="'.OC_Util::sanitizeHTML($timezone).'"'.($_['timezone'] == $timezone?' selected="selected"':'').'>'.OC_Util::sanitizeHTML($city).'</option>');
-								endforeach;?>
-								</select>
-							</td>
-						</tr>
-						<tr>
-							<td>
-								<input type="checkbox" name="timezonedetection" id="timezonedetection">
-								<label for="timezonedetection"><?php p($l->t('Update timezone automatically')); ?></label>
-							</td>
-						</tr>
-						<tr>
-							<td>
-								<label for="timeformat" class="bold"><?php p($l->t('Time format'));?></label>
-							</td>
-						</tr>
-						<tr>
-							<td>
-								<select id="timeformat" title="<?php p("timeformat"); ?>" name="timeformat">
-									<option value="24" id="24h"><?php p($l->t("24h")); ?></option>
-									<option value="ampm" id="ampm"><?php p($l->t("12h")); ?></option>
-								</select>
-							</td>
-						</tr>
-						<tr>
-							<td>
-								<label for="firstday" class="bold"><?php p($l->t('Start week on'));?></label>
-								&nbsp;&nbsp;
-							</td>
-						</tr>
-						<tr>
-							<td>
-								<select id="firstday" title="<?php p("First day"); ?>" name="firstday">
-									<option value="mo" id="mo"><?php p($l->t("Monday")); ?></option>
-									<option value="su" id="su"><?php p($l->t("Sunday")); ?></option>
-									<option value="sa" id="sa"><?php p($l->t("Saturday")); ?></option>
-								</select>
-							</td>
-						</tr>
-						<tr class="advancedsettings">
-							<td>
-								<label class="bold"><?php p($l->t('Cache'));?></label>
-							</td>
-						</tr>
-						<tr>
-							<td>
-								<input id="cleancalendarcache" type="button" class="button" value="<?php p($l->t('Clear cache for repeating events'));?>">
-							</td>
-						</tr>
-					</table>
-				</div>
-				<h2 id="title_urls"><?php p($l->t('URLs')); ?></h2>
-				<div id="urls">
-						<?php p($l->t('Calendar CalDAV syncing addresses')); ?> (<a href="http://owncloud.org/synchronisation/" target="_blank" class="link"><?php p($l->t('more info')); ?></a>)
-						<dl>
-						<dt><?php p($l->t('Primary address (Kontact et al)')); ?></dt>
-						<dd><input type="text" style="width: 90%;float: left;" value="<?php print_unescaped(OCP\Util::linkToRemote('caldav')); ?>" readonly></dd>
-						<dt><?php p($l->t('iOS/OS X')); ?></dt>
-						<dd><input type="text" style="width: 90%;float: left;" value="<?php print_unescaped(OCP\Util::linkToRemote('caldav')); ?>principals/<?php p(OCP\USER::getUser()); ?>/" readonly></dd>
-						<dt><?php p($l->t('Read only iCalendar link(s)')); ?></dt>
-						<dd>
-							<?php foreach($_['calendars'] as $calendar) {
-							if($calendar['userid'] == OCP\USER::getUser()){
-								$uri = rawurlencode(html_entity_decode($calendar['uri'], ENT_QUOTES, 'UTF-8'));
-							}else{
-								$uri = rawurlencode(html_entity_decode($calendar['uri'], ENT_QUOTES, 'UTF-8')) . '_shared_by_' . $calendar['userid'];
+		<div id="app-settings-header">
+			<button class="settings-button generalsettings" tabindex="0"></button>
+		</div>
+		<div id="app-settings-content">
+			<ul>
+				<li>
+					<label for="timezone" class="bold"><?php p($l->t('Timezone')); ?></label>
+					<select id="timezone" name="timezone">
+						<?php
+						$continent = '';
+						foreach ($_['timezones'] as $timezone):
+							$ex = explode('/', $timezone, 2); //obtain continent,city
+							if (!isset($ex[1])) {
+								$ex[1] = $ex[0];
+								$ex[0] = "Other";
 							}
-							?>
-							<a href="<?php p(OCP\Util::linkToRemote('caldav').'calendars/'.OCP\USER::getUser().'/'.$uri) ?>?export" class="link"><?php p(OCP\Util::sanitizeHTML($calendar['displayname'])) ?></a><br />
-							<?php } ?>
-						</dd>
-						</dl>
-					</div>
-				</div>
-			</div>
-		<!-- End of settings -->
+							if ($continent != $ex[0]):
+								if ($continent != "")
+									print_unescaped('</optgroup>');
+								print_unescaped('<optgroup label="' . OC_Util::sanitizeHTML($ex[0]) . '">');
+							endif;
+							$city = strtr($ex[1], '_', ' ');
+							$continent = $ex[0];
+							print_unescaped('<option value="' . OC_Util::sanitizeHTML($timezone) . '"' . ($_['timezone'] == $timezone ? ' selected="selected"' : '') . '>' . OC_Util::sanitizeHTML($city) . '</option>');
+						endforeach;
+						?>
+					</select>
+
+				</li>
+				<li>
+					<input type="checkbox" name="timezonedetection" id="timezonedetection">
+					<label for="timezonedetection"><?php p($l->t('Update timezone automatically')); ?></label>
+				</li>
+				<li>
+					<label for="timeformat" class="bold"><?php p($l->t('Time format')); ?></label>
+					<select id="timeformat" title="<?php p("timeformat"); ?>" name="timeformat">
+						<option value="24" id="24h"><?php p($l->t("24h")); ?></option>
+						<option value="ampm" id="ampm"><?php p($l->t("12h")); ?></option>
+					</select>
+				</li>
+				<li>
+					<label for="firstday" class="bold"><?php p($l->t('Start week on')); ?></label>
+					<select id="firstday" title="<?php p("First day"); ?>" name="firstday">
+						<option value="mo" id="mo"><?php p($l->t("Monday")); ?></option>
+						<option value="su" id="su"><?php p($l->t("Sunday")); ?></option>
+						<option value="sa" id="sa"><?php p($l->t("Saturday")); ?></option>
+					</select>
+				</li>
+				<li>
+					<label class="bold"><?php p($l->t('Cache')); ?></label>
+					<input id="cleancalendarcache" type="button" class="button" value="<?php p($l->t('Clear cache for repeating events')); ?>">
+				</li>
+				<li>
+					<label class="bold"><?php p($l->t('Primary CalDAV address')); ?></label>
+					<input type="text" value="<?php print_unescaped(OCP\Util::linkToRemote('caldav')); ?>" style="width:90%" readonly>
+				</li>
+				<li>
+					<label class="bold"><?php p($l->t('iOS/OS X CalDAV address')); ?></label>
+					<input type="text" value="<?php print_unescaped(OCP\Util::linkToRemote('caldav')); ?>principals/<?php p(OCP\USER::getUser()); ?>/" style="width:90%" readonly>
+				</li>
+			</ul>
+		</div>
+	</div>
+	<!-- End of settings -->
 </div>
 <div id="fullcalendar"><img id="loading" src="<?php print_unescaped(OCP\Util::imagePath('calendar', 'loading.gif')); ?>" /></div>
 <div id="dialog_holder"></div>
