ccce9aa2b183aa75716f61d3334b0af734bd147b
jbtbnl <jbtbnl@users.noreply.github.com>
2014-06-10 11:48:49 +0200

    Include #443

--- templates/calendar.php
+++ templates/calendar.php
@@ -111,7 +111,8 @@
 	</div>
 	<!-- End of settings -->
 </div>
-<div id="fullcalendar"><div id="loading" class="icon-loading"></div></div>
-<div id="dialog_holder"></div>
-<div id="appsettings" class="popup topright hidden"></div>
+<div id="app-content">
+	<div id="fullcalendar"><div id="loading" class="icon-loading"></div></div>
+	<div id="dialog_holder"></div>
+</div>
 <input type="hidden" name="allowShareWithLink" id="allowShareWithLink" value="<?php p($_['allowShareWithLink']) ?>" />
