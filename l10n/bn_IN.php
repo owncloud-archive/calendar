cae865cf195b9256abc68a06a76380f4024f20cc
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-07-17 01:46:07 -0400

    [tx-robot] updated from transifex

---
+++ l10n/bn_IN.php
@@ -0,0 +1,9 @@
+<?php $TRANSLATIONS = array(
+"Daily" => "প্রতিদিন",
+"Weekly" => "সাপ্তাহিক",
+"Date" => "তারিখ",
+"Edit" => "এডিট",
+"Delete" => "মুছে ফেলা",
+"Share" => "শেয়ার",
+"Import" => "ইম্পোর্ট"
+);
