fbbb4f6fbae0f55f634d8d699059aa112c62a67f
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-08-17 01:46:31 -0400

    [tx-robot] updated from transifex

--- l10n/az.php
+++ l10n/az.php
@@ -1,4 +1,13 @@
 <?php $TRANSLATIONS = array(
+"Not all calendars are completely cached" => "Bütün kalendarlar tamlıqla cache edilməyib",
+"Everything seems to be completely cached" => "Hər çey tam cache-lənmiş görünür",
+"No calendars found." => "Heç bir kalendar tapılmadı",
+"No events found." => "Heç bir xəbərdarlıq tapılmadı",
+"Wrong calendar" => "Səhv kalendar",
 "Daily" => "Günlük",
-"Weekly" => "Həftəlik"
+"Weekly" => "Həftəlik",
+"Date" => "Tarix",
+"Share" => "Yayımla",
+"Description" => "Açıqlanma",
+"Import" => "Əlavə et"
 );
